6ee891b50251b67a54f02f6a1469d91986b231ef
Ng Guoyou <ng.guoyou@gmail.com>
2018-03-21 11:25:18 +0800

    Add port number to RPC address for gateway in parity demo docker deployment script

--- docker/run_server.parity_demo.sh
+++ docker/run_server.parity_demo.sh
@@ -8,4 +8,4 @@ yarn truffle deploy --network parity_demo
 yarn --silent cli contract getaddress --network parity_demo > /tmp/address
 yarn --silent cli account getaccount --network parity_demo > /tmp/account
 echo "Address $(tail -1 /tmp/address)"
-node /usr/src/app/gateway/build --networkId 0x4242 --endpoint "http://10.7.32.156" --contractAddress $(tail -1 /tmp/address) --agent $(tail -1 /tmp/account)
+node /usr/src/app/gateway/build --networkId 0x4242 --endpoint "http://10.7.32.156:8545" --contractAddress $(tail -1 /tmp/address) --agent $(tail -1 /tmp/account)
